1536f0597258376033d01d1aa33403f1f0750e44
xiaof <xffit@163.com>
2019-12-08 23:37:28 +0800

    debug level

--- demomongodb/src/main/java/com/example/demomongodb/controller/Hello.java
+++ demomongodb/src/main/java/com/example/demomongodb/controller/Hello.java
@@ -1,6 +1,8 @@
 package com.example.demomongodb.controller;
 
 import com.example.demomongodb.service.WriteIPStats;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,8 +14,13 @@ public class Hello {
     @Autowired
     private WriteIPStats writeIPStats;
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String helloWorld(){
+        logger.info("hello");
+        logger.debug("this is debug");
+
         return "hello world";
     }
 
